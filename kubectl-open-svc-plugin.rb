9d3b490a607eb83729ea95f6eba6e749053561a2
Kazuki Suda <kazuki.suda@gmail.com>
2018-09-30 15:40:47 +0900

    Bump up the version to v2.0.0

--- kubectl-open-svc-plugin.rb
+++ kubectl-open-svc-plugin.rb
@@ -1,19 +1,20 @@
 class KubectlOpenSvcPlugin < Formula
   desc "A kubectl plugin that open the Kubernetes URL(s) for the specified service in your browser."
   homepage "https://github.com/superbrothers/kubectl-open-svc-plugin"
-  version "v1.2.0"
-  url "https://github.com/superbrothers/kubectl-open-svc-plugin/releases/download/#{version}/open-svc-darwin-amd64.zip"
-  sha256 "92e21f890c8fed584fe16f4e7cfbb707c8fbc95de286422e06a66ac550040ac5"
+  version "v2.0.0"
+  url "https://github.com/superbrothers/kubectl-open-svc-plugin/releases/download/#{version}/kubectl-open_svc-darwin-amd64.zip"
+  sha256 "8b8c17a9591473a7a42973dd439c0bed3476bee49f72db3753fbb73e81a50ff6"
 
   def install
+    bin.install "kubectl-open_svc"
     prefix.install Dir["*"]
   end
 
   def caveats; <<~EOS
-    Run the following commands in terminal:
-      mkdir -p ~/.kube/plugins && \\
-      rm -rf ~/.kube/plugins/kubectl-open-svc-plugin && \\
-      cp -RH $(brew --prefix kubectl-open-svc-plugin) ~/.kube/plugins/
+    Note that kubectl-open-svc v2.0.0 only supports kubectl v1.12 and more.
+    If you are using kubectl v1.11, you can install kubectl-open-svc v1.2.0 from the release page below.
+
+    https://github.com/superbrothers/kubectl-open-svc-plugin/releases/tag/v1.2.0
     EOS
   end
 end
